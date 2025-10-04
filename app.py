7e99deb4aa8c7148ba58b58e09263ec72fe866dd
krishnaditi <krishanaaditi@gmail.com>
2025-10-04 15:25:42 +0530

    refactor app.py to include multiple models and encapsulate admin creation logic

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from models import db, User
+from models import db, Admin, Customer, Seller, Storage
 
 app = Flask(__name__)
 
@@ -8,15 +8,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///aditi.db'
 
 db.init_app(app)
 
-with app.app_context():
-    db.create_all()
+#predefing admin credentials
+def create_admin():
+    with app.app_context():
+        db.create_all()  # Create tables if they don't exist
+        admin = Admin.query.filter_by(username='admin').first()
+        if not admin:
+            admin = Admin(username='admin', email='admin@example.com', contact=1234567890, password='admin123')
+            db.session.add(admin)
+            db.session.commit()
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-
-
 if __name__ == '__main__':
+    create_admin()
     app.run(debug=True, port=5001)
 
